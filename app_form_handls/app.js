0f4a660986f3d965923fb3bcde9b50666c269226
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2024-05-13 15:18:03 -0300

    Condiçoes no handlebar

--- app_form_handls/app.js
+++ app_form_handls/app.js
@@ -3,11 +3,17 @@ import exphbs from 'express-handlebars';
 
 const app = express();
 
+const pessoas = [
+  { nome: 'Gabriel', idade: 28 },
+  { nome: 'Amanda', idade: 32 },
+  { nome: 'Otavio', idade: 18 },
+];
+
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'principal' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.render('inicio');
+  res.render('inicio', { gostandoDeNode: false, dados: pessoas });
 });
 
 app.get('/sobre', (req, res) => {
